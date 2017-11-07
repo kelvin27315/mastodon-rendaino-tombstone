f2b66cd34f9ae982ac0d4ce291f541a80a71ad4f
ide-an <ideanweb@gmail.com>
2017-11-07 23:06:14 +0900

    fix typo

--- test_Round_tombstone.py
+++ test_Round_tombstone.py
@@ -54,33 +54,33 @@ class TestRoundTombstone(unittest.TestCase):
         self.assertEqual(['霊夢','魔理沙'], list(results['display_name']))
 
 # toot_number_rotatedについて、細かいケースごとにテスト
-    def test_toot_number_rotated_no_partitian(self):
+    def test_toot_number_rotated_no_participation(self):
         rt.toot_number_rotated(0, 0, 0)
         rt.mastodon.toot.assert_called_once_with(TODAY_STR + 'の墓石は回転しませんでした。')
 
-    def test_toot_number_rotated_1_partitian(self):
+    def test_toot_number_rotated_1_participation(self):
         rt.toot_number_rotated(1, 0, 0)
 # BUG: 4人未満の場合に余計な0が付く
         rt.mastodon.toot.assert_called_once_with(TODAY_STR + 'の墓石の回転は1人による04分の1回転でした。')
 
-    def test_toot_number_rotated_2_partitians(self):
+    def test_toot_number_rotated_2_participations(self):
         rt.toot_number_rotated(2, 0, 0)
 # BUG: 4人未満の場合に余計な0が付く
         rt.mastodon.toot.assert_called_once_with(TODAY_STR + 'の墓石の回転は2人による02分の1回転でした。')
 
-    def test_toot_number_rotated_4_partitians(self):
+    def test_toot_number_rotated_4_participations(self):
         rt.toot_number_rotated(4, 0, 0)
         rt.mastodon.toot.assert_called_once_with(TODAY_STR + 'の墓石の回転は4人による1回転でした。')
 
-    def test_toot_number_rotated_5_partitians(self):
+    def test_toot_number_rotated_5_participations(self):
         rt.toot_number_rotated(5, 0, 0)
         rt.mastodon.toot.assert_called_once_with(TODAY_STR + 'の墓石の回転は5人による1と4分の1回転でした。')
 
-    def test_toot_number_rotated_6_partitians(self):
+    def test_toot_number_rotated_6_participations(self):
         rt.toot_number_rotated(6, 0, 0)
         rt.mastodon.toot.assert_called_once_with(TODAY_STR + 'の墓石の回転は6人による1と2分の1回転でした。')
 
-    def test_toot_number_rotated_early_partitian(self):
+    def test_toot_number_rotated_early_participation(self):
         rt.toot_number_rotated(6, 1, 0)
         rt.mastodon.toot.assert_called_once_with(TODAY_STR + 'の墓石の回転は6人による1と2分の1回転でした。\nまた、2時30分になる前に回した人は1人です。')
 
@@ -88,7 +88,7 @@ class TestRoundTombstone(unittest.TestCase):
         rt.toot_number_rotated(6, 0, 1)
         rt.mastodon.toot.assert_called_once_with(TODAY_STR + 'の墓石の回転は6人による1と2分の1回転でした。2度以上回した人は1人です。')
 
-    def test_toot_number_rotated_early_partition_and_multi_turn(self):
+    def test_toot_number_rotated_early_participation_and_multi_turn(self):
         rt.toot_number_rotated(6, 1, 1)
         rt.mastodon.toot.assert_called_once_with(TODAY_STR + 'の墓石の回転は6人による1と2分の1回転でした。\nまた、2時30分になる前に回した人は1人、2度以上回した人は1人です。')
 
