c3786757cb181e1a954d77730a732fb64092d64c
kelvin27315 <seven_units@yahoo.co.jp>
2018-01-13 03:49:45 +0900

    時刻周りの判定を修正しました。それに合わせてtest_Round_tombstone.pyも書き換えました。

--- test_Round_tombstone.py
+++ test_Round_tombstone.py
@@ -19,26 +19,25 @@ class TestRoundTombstone(unittest.TestCase):
 
     def test_get_timeline(self):
         rt.mastodon.timeline.side_effect = [
-# timeline取得 1回目
+ # timeline取得 1回目
             [
-                { 'id': 10, 'created_at': datetime(2017, 11, 4, 17, 29, 0, 0, UTC), 'content': 'ズズズ', 'account': { 'username': 'reimu', 'display_name': '霊夢', } },
-                { 'id': 11, 'created_at': datetime(2017, 11, 4, 17, 30, 0, 0, UTC), 'content': 'ｽﾞｽﾞｽﾞ', 'account': { 'username': 'marisa', 'display_name': '魔理沙', } },
                 { 'id': 12, 'created_at': datetime(2017, 11, 4, 17, 30, 0, 0, UTC), 'content': 'コスズ', 'account': { 'username': 'kosuzu', 'display_name': '小鈴', } },
+                { 'id': 11, 'created_at': datetime(2017, 11, 4, 17, 30, 0, 0, UTC), 'content': 'ｽﾞｽﾞｽﾞ', 'account': { 'username': 'marisa', 'display_name': '魔理沙', } },
+                { 'id': 10, 'created_at': datetime(2017, 11, 4, 17, 29, 0, 0, UTC), 'content': 'ズズズ', 'account': { 'username': 'reimu', 'display_name': '霊夢', } }
             ],
 # timeline取得 2回目
             [
-                { 'id': 8, 'created_at': datetime(2017, 11, 4, 17, 29, 0, 0, UTC), 'content': 'ズズズ', 'account': { 'username': 'sakuya', 'display_name': '咲夜', } },
                 { 'id': 9, 'created_at': datetime(2017, 11, 4, 17, 29, 0, 0, UTC), 'content': 'ズズズ', 'account': { 'username': 'sanae', 'display_name': '早苗', } },
+                { 'id': 8, 'created_at': datetime(2017, 11, 4, 17, 28, 0, 0, UTC), 'content': 'ズズズ', 'account': { 'username': 'sakuya', 'display_name': '咲夜', } },
             ],
 # timeline取得 3回目。実際には呼ばれない
-# BUG: 29:00.000を含めるかどうかがget_timelineとselect_tootsで一致していない
             [
-                { 'id': 7, 'created_at': datetime(2017, 11, 4, 17, 29, 0, 0, UTC), 'content': 'ズズズ', 'account': { 'username': 'akyu', 'display_name': '阿求', } },
+                { 'id': 7, 'created_at': datetime(2017, 11, 4, 17, 28, 0, 0, UTC), 'content': 'ズズズ', 'account': { 'username': 'akyu', 'display_name': '阿求', } },
             ],
         ]
         results = rt.get_timeline('local')
 # 取得したTLの検証。
-        self.assertEqual(['咲夜', '早苗', '霊夢', '魔理沙', '小鈴'], [x['account']['display_name'] for x in results])
+        self.assertEqual(['咲夜', '霊夢', '早苗', '小鈴', '魔理沙'], [x['account']['display_name'] for x in results])
 # mastodon APIの呼び出し回数検証
         self.assertEqual(2, len(rt.mastodon.timeline.mock_calls))
 
@@ -48,7 +47,7 @@ class TestRoundTombstone(unittest.TestCase):
             { 'id': 10, 'created_at': datetime(2017, 11, 4, 17, 29, 0, 0, UTC), 'content': 'ズズズ', 'account': { 'username': 'reimu', 'display_name': '霊夢', } },
             { 'id': 11, 'created_at': datetime(2017, 11, 4, 17, 30, 0, 0, UTC), 'content': 'ｽﾞｽﾞｽﾞ', 'account': { 'username': 'marisa', 'display_name': '魔理沙', } },
 # negative
-            { 'id': 12, 'created_at': datetime(2017, 11, 4, 17, 30, 0, 0, UTC), 'content': 'コスズ', 'account': { 'username': 'kosuzu', 'display_name': '小鈴', } },
+            { 'id': 12, 'created_at': datetime(2017, 11, 4, 17, 31, 0, 0, UTC), 'content': 'コスズ', 'account': { 'username': 'kosuzu', 'display_name': '小鈴', } },
         ]
         results = rt.select_toots(toots)
         self.assertEqual(['reimu','marisa'], list(results['username']))
